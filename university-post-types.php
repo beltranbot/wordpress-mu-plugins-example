f5c6c2e61199b8140ee7c5c63873c03887ddb380
beltranbot <beltranbot@gmail.com>
2022-09-18 20:55:51 -0500

    adding support for modern editor and excerpts for events

--- university-post-types.php
+++ university-post-types.php
@@ -3,10 +3,11 @@
 function university_post_types()
 {
     register_post_type("event", array(
+        "supports" => Array("title" ,"editor", "excerpt"), // editor needs to be added to use new editor
         "rewrite" => Array("slug" => "events"), // overwrites urls
         "has_archive" => true, // restructure permalinks for this to work
         "public" => true, //
-        "show_in_rest" => false, // set to true to use the new editor
+        "show_in_rest" => true, // set to true to use the new editor
         "labels" => array(
             "name" => "Events",
             "add_new_item" => "Add New Event",
