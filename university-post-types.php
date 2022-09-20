3f7a7a3a8996ca98efcfd56576871b2d32ca5dfa
beltranbot <beltranbot@gmail.com>
2022-09-19 21:50:38 -0500

    adding program post type

--- university-post-types.php
+++ university-post-types.php
@@ -2,6 +2,7 @@
 
 function university_post_types()
 {
+    // event post type
     register_post_type("event", array(
         "supports" => Array("title" ,"editor", "excerpt"), // editor needs to be added to use new editor
         "rewrite" => Array("slug" => "events"), // overwrites urls
@@ -16,6 +17,23 @@ function university_post_types()
             "singular_name" => "Event"
         ),
         "menu_icon" => "dashicons-calendar" // wordpress dashicons
+    ));
+
+    // Program post type
+    register_post_type("program", array(
+        "supports" => Array("title" ,"editor"), // editor needs to be added to use new editor
+        "rewrite" => Array("slug" => "programs"), // overwrites urls
+        "has_archive" => true, // restructure permalinks for this to work
+        "public" => true, //
+        "show_in_rest" => true, // set to true to use the new editor
+        "labels" => array(
+            "name" => "Programs",
+            "add_new_item" => "Add New Program",
+            "edit_item" => "Edit Program",
+            "all_items" => "All Programs",
+            "singular_name" => "Program"
+        ),
+        "menu_icon" => "dashicons-awards" // wordpress dashicons
     ));
 }
 
